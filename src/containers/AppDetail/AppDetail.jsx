89c6b6e9b014af88332fb6de639fb5699f9bcf1d
prashant yadav <prashant.yadav567@gmail.com>
2018-08-27 02:01:28 +0530

    Added freq feature

--- src/containers/AppDetail/AppDetail.jsx
+++ src/containers/AppDetail/AppDetail.jsx
@@ -12,6 +12,22 @@ class AppDetail extends Component {
         this.props.getAppDetail(this.props.match.params.id);
     }
 
+    componentWillUpdate(nextProps) {
+        if (nextProps.apps && nextProps.apps.result && nextProps.apps.result.length === 1) {
+            let freqUsedApps = localStorage.getItem('freqUsedApps');
+            if (freqUsedApps !== '' && freqUsedApps !== null) {
+                let apps = JSON.parse(freqUsedApps);
+                apps = apps.filter((item, index) => {
+                    return item.id !== this.props.match.params.id;
+                });
+                apps.unshift(nextProps.apps.result[0]);
+                localStorage.setItem('freqUsedApps', JSON.stringify(apps));
+            } else {
+                localStorage.setItem('freqUsedApps', JSON.stringify([nextProps.apps.result[0]]));
+            }
+        }
+    }
+
     render() {
         return (
             this.props.apps && this.props.apps.result && this.props.apps.result.length === 1 ? <CAppDetail {...this.props.apps.result[0]} /> : ''
